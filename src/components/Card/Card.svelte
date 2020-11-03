392e84897434f8d0fb3a74c5db6e37cd82e2644a
Jakob <jade@likeavulin.dk>
2020-11-03 16:50:03 +0100

    generic card

---
+++ src/components/Card/Card.svelte
@@ -0,0 +1,55 @@
+<script>
+  export let img =
+    "https://cdn.pixabay.com/photo/2020/09/18/12/16/jewel-beetle-5581683_1280.jpg";
+  export let likes = 0;
+  export let comments = 0;
+  export let lastchange = 0;
+
+//   let url_default =
+//     "https://storagejakob.blob.core.windows.net/blob-container-test/default-horse.jpg";
+</script>
+
+<style>
+  .missing {
+    color: #999;
+  }
+
+  /* On mouse-over, add a deeper shadow */
+  ion-card:hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.8);
+  }
+</style>
+
+<ion-card>
+  <img src={img} alt="avatar" />
+
+  <ion-card-content>
+    <p>
+      <slot name="content">
+        <p class="missing">No content</p>
+      </slot>
+    </p>
+  </ion-card-content>
+
+  <ion-row>
+    <ion-col>
+      <ion-button icon-start fill="clear" size="small">
+        <ion-icon name="thumbs-up-outline" />
+        <div>{likes} Likes</div>
+      </ion-button>
+    </ion-col>
+    <ion-col>
+      <ion-button icon-start fill="clear" size="small">
+        <ion-icon name="chatbubbles-outline" />
+        <div>{comments} Comments</div>
+      </ion-button>
+    </ion-col>
+    <ion-col>
+      <ion-button icon-start fill="clear" size="small">
+        <ion-icon name="pulse-outline" size="small" />
+        <div>{lastchange}h ago</div>
+      </ion-button>
+    </ion-col>
+  </ion-row>
+
+</ion-card>
--- src/pages/Items.svelte
+++ src/pages/Items.svelte
@@ -1,52 +1,33 @@
 <script>
-  import { blur, slide, scale, fade, fly } from 'svelte/transition';
-  let url_default = "https://storagejakob.blob.core.windows.net/blob-container-test/default-horse.jpg"
-
+  import { blur, slide, scale, fade, fly } from "svelte/transition";
+  import Card from "../components/Card/Card.svelte";
 </script>
+
 <ion-content class="app-page">
 
-<ion-card>
-    <img src="https://cdn.pixabay.com/photo/2020/10/27/14/59/deer-5691010_1280.jpg" alt="dyr" />
-    <ion-card-header>
-      <ion-card-subtitle>Arkitetk</ion-card-subtitle>
-      <ion-card-title>John Doe</ion-card-title>
-    </ion-card-header>
-    <ion-card-content>
-      Founded in 1829 on an isthmus between Lake Monona and Lake Mendota,
-      Madison was named the capital of the Wisconsin Territory in 1836.
-    </ion-card-content>
-  </ion-card>
+  <Card
+    likes="4"
+    img="https://cdn.pixabay.com/photo/2020/10/10/21/54/performers-5644247_1280.jpg">
+    <span slot="content">Først rejste jeg tilbage og så frem igen.. crazy</span>
+  </Card>
 
-<ion-card>
-    <img src="https://cdn.pixabay.com/photo/2020/09/18/12/16/jewel-beetle-5581683_1280.jpg" alt="dyr 2" />
-    <ion-card-header>
-      <ion-card-subtitle>Development</ion-card-subtitle>
-      <ion-card-title>Jane</ion-card-title>
-    </ion-card-header>
-    <ion-card-content>
-      Founded in 1829 on an isthmus between Lake Monona and Lake Mendota,
-      Madison was named the capital of the Wisconsin Territory in 1836.
-    </ion-card-content>
-  </ion-card>
+  <Card
+    likes="5"
+    comments="10"
+    lastchange="3"
+    img="https://cdn.pixabay.com/photo/2020/09/18/12/16/jewel-beetle-5581683_1280.jpg">
+    <span slot="content">Capital of the Wisconsin</span>
+  </Card>
 
-  <ion-card>
-    <img src="https://cdn.pixabay.com/photo/2020/10/31/20/23/lighthouse-5702233_1280.jpg" alt="ikke dyr" />
-    <ion-card-header>
-      <ion-card-subtitle>Wiz</ion-card-subtitle>
-      <ion-card-title>J</ion-card-title>
-    </ion-card-header>
-    <ion-card-content>
+  <Card
+    likes="5"
+    comments="10"
+    lastchange="3"
+    img="https://cdn.pixabay.com/photo/2020/10/27/14/59/deer-5691010_1280.jpg">
+    <span slot="content">
       Founded in 1829 on an isthmus between Lake Monona and Lake Mendota,
       Madison was named the capital of the Wisconsin Territory in 1836.
-    </ion-card-content>
-  </ion-card>
+    </span>
+  </Card>
+
 </ion-content>
-
-<style>
-
-/* On mouse-over, add a deeper shadow */
-ion-card:hover {
-  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.8);
-}
-
-</style>
